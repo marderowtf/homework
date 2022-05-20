0219471d88e19c313397857b6caa14e2da2f30b7
mardero <pentogramma@gmail.com>
2022-05-20 12:38:41 +0300

    update script for prometheus_nginx

--- 7-nginx-prometheus.sh
+++ 7-nginx-prometheus.sh
@@ -5,7 +5,7 @@ wget https://github.com/nginxinc/nginx-prometheus-exporter/releases/download/v0.
 tar xzvf nginx-prometheus-exporter_0.10.0_linux_amd64.tar.gz
 mv nginx-prometheus-exporter /usr/local/bin
 useradd -r nginx_exporter
-cp /tmp/node_exporter.service /etc/systemd/system/
+cp /tmp/homework/node_exporter.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl start node_exporter
 systemctl enable node_exporter

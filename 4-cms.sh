147bb5b768ea2e2dd7b7a3945e0344be41f5214d
mardero <pentogramma@gmail.com>
2022-05-20 15:49:40 +0300

    update script for wordpress

--- 4-cms.sh
+++ 4-cms.sh
@@ -6,7 +6,7 @@ yum --enablerepo=remi-php74 install php php-pdo php-fpm php-gd php-mbstring php-
 php -v
 wget https://ru.wordpress.org/latest-ru_RU.zip
 unzip latest-ru_RU.zip
-cp -r /tmp/wordpress/ /var/www/wordpress/
+cp -r /tmp/homework/wordpress/ /var/www/wordpress/
 chmod -R 777 /var/www/wordpress/wp-content
 chown -R apache. wordpress
 systemctl restart httpd

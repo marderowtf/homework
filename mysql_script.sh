c9eb58bb292919b02c5686562611ab64622d0b73
mardero <pentogramma@gmail.com>
2022-05-20 12:41:19 +0300

    update script for backup mysql

--- mysql_script.sh
+++ mysql_script.sh
@@ -6,15 +6,15 @@ PATH=$PATH:/usr/local/bin
 
 DIR=`date +"%Y-%m-%d"`
 DATE=`date +"%Y%m%d"`
-MYSQL='mysql --defaults-extra-file=/home/root/user.cnf --skip-column-names'
+MYSQL='mysql --defaults-extra-file=/root/user.cnf --skip-column-names'
 
 for skull in `$MYSQL -e "SHOW DATABASES"`;
 do
-	if [ $skull != "information_schema" ] && [ $skull != "mysql" ] && [ $skull != "sys" ] && [ $skull != "performance_schema" ]; then
+	if [ $skull != "information_schema" ] && [ $skull != "wordpress" ] && [ $skull != "sys" ] && [ $skull != "performance_schema" ]; then
 		for table in `$MYSQL $skull -e "SHOW TABLES"`;
 		do	
 		    mkdir -p $skull
-		/usr/bin/mysqldump --defaults-extra-file=/home/mardero/user.cnf --add-drop-table --add-locks --create-options --disable-keys --extended-insert --single-transaction --quick --set-charset --events --routines --triggers --master-data $skull $table | gzip -1 > $skull/$table.gz;	
+		/usr/bin/mysqldump --defaults-extra-file=/root/user.cnf --add-drop-table --add-locks --create-options --disable-keys --extended-insert --single-transaction --quick --set-charset --events --routines --triggers --master-data $skull $table | gzip -1 > $skull/$table.gz;	
 		done
 	fi
 done

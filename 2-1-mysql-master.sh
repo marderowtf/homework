58adc9f6b00be31453f13035b025d88b4f40cb6f
mardero <pentogramma@gmail.com>
2022-05-20 14:08:51 +0300

    update script for master

--- 2-1-mysql-master.sh
+++ 2-1-mysql-master.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Mysql Create user to master
+# Mysql Create db user to master
 cp /tmp/homework/my.cnf /etc/
 mysql <<EOF
 ALTER USER 'root'@'localhost' IDENTIFIED WITH 'caching_sha2_password' BY 'G@pbnbd!23';
 CREATE USER repl@'%' IDENTIFIED WITH 'caching_sha2_password' BY 'oTUSlave#2020';
 GRANT REPLICATION SLAVE ON *.* TO repl@'%';
+CREATE DATABASE otus;
+SHOW DATABASES;
 SHOW MASTER STATUS;
 SELECT User, Host FROM mysql.user;
 EOF

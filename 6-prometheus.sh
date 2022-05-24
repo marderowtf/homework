70f0cbfc81f16fd9a259097d187067936674f9ab
mardero <pentogramma@gmail.com>
2022-05-24 16:40:11 +0300

    update script for prometheus

--- 6-prometheus.sh
+++ 6-prometheus.sh
@@ -70,9 +70,9 @@ User=prometheus
 Group=prometheus
 Type=simple
 ExecStart=/usr/local/bin/prometheus
---config.file /etc/prometheus/prometheus.yml
---storage.tsdb.path /var/lib/prometheus/
---web.console.templates=/etc/prometheus/consoles
+--config.file /etc/prometheus/prometheus.yml \
+--storage.tsdb.path /var/lib/prometheus/ \
+--web.console.templates=/etc/prometheus/consoles \
 --web.console.libraries=/etc/prometheus/console_libraries
 ExecReload=/bin/kill -HUP $MAINPID
 

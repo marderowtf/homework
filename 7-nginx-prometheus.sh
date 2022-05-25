b06ea7859294ec5b32fd5576b8c22cfb68df132b
mardero <pentogramma@gmail.com>
2022-05-25 10:20:47 +0300

    add update nginx-prometheus

--- 7-nginx-prometheus.sh
+++ 7-nginx-prometheus.sh
@@ -5,7 +5,26 @@ wget https://github.com/nginxinc/nginx-prometheus-exporter/releases/download/v0.
 tar xzvf nginx-prometheus-exporter_0.10.0_linux_amd64.tar.gz
 mv nginx-prometheus-exporter /usr/local/bin
 useradd -r nginx_exporter
-cp /tmp/homework/node_exporter.service /etc/systemd/system/
+chown nginx_exporter:nginx_exporter /usr/local/bin/nginx-prometheus-exporter
+cat > /etc/systemd/system/nginx_prometheus_exporter.service <<EOF
+[Unit]
+Description=NGINX Prometheus Exporter
+After=network.target
+
+[Service]
+Type=simple
+User=nginx_exporter
+Group=nginx_exporter
+ExecStart=/usr/local/bin/nginx-prometheus-exporter \
+    -web.listen-address=192.168.8.12:9113 \
+    -nginx.scrape-uri http://127.0.0.1:81/metrics
+
+SyslogIdentifier=nginx_prometheus_exporter
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
 systemctl daemon-reload
-systemctl start node_exporter
-systemctl enable node_exporter
+systemctl start nginx_prometheus_exporter.service
+systemctl enable nginx_prometheus_exporter.service
